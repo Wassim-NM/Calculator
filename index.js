29622c2b3f83663d0f34d0aff6a5beb10c11ee35
Wassim <wassimmahmoud200500@gmail.com>
2024-12-14 22:49:38 +0200

    Project semi-finished

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const inputs_html = document.getElementById('inputs');
+const result_html = document.querySelector('#result');
 let input;
+let result;
 
 let btns = document.querySelectorAll('button');
-btns.forEach(displayOnClick);
+// btn = full item of the list of btns
+btns.forEach((btn) => {
+    btn.addEventListener('click', calculate)
+});
 
-function displayOnClick(element) {
-    element.addEventListener('click', ()=> {
-        input = element.innerText;
-        if (input == 'AC') {
-            inputs_html.innerHTML = '';
-        }else if (input == 'Del') {
-            inputs_html.innerHTML[-1] = '';
-        }else{
-            inputs_html.innerHTML += input;
-        }
-    });
-};
+function calculate(event) {
+    let btn_text = this.innerText;  // first method
+    // let btn_text = event.target.value; // second method
+    // if (btn_text != '(' && ')' && '/' && '*' && '-' && '+' && '=' && 'Del' && 'AC') {
+    //     inputs_html.innerText += btn_text;
+    // };
+    if (btn_text == 'AC') {
+        inputs_html.innerHTML = '';
+        result_html.innerHTML = '0';
+    } else if (btn_text == '=') {
+        result = eval(inputs_html.innerText);
+        result_html.innerText = result;
+    } else if (btn_text == 'Del') {
+
+    } else {
+        inputs_html.innerText += btn_text;
+    };
+}
+
+// function displayOnClick(element) {
+//     element.addEventListener('click', ()=> {
+//         input = element.innerText;
+//         if (input == 'AC') {
+//             inputs_html.innerHTML = '';
+//         }else if (input == 'Del') {
+//             inputs_html.innerHTML[-1] = '';
+//         }else{
+//             inputs_html.innerHTML += input;
+//         }
+//     });
+// };
